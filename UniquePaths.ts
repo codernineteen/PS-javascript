622991f0b9e639fdaceaed5a5e3e9ee8490ca581
전예찬 <wjsdP@Air-MacBook.local>
2022-09-09 14:57:38 +0900

    Find unique paths to reach goal : combination solution

---
+++ UniquePaths.ts
@@ -0,0 +1,19 @@
+/*
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+ */
+
+//option: right or down
+function uniquePaths(m: number, n: number): number {
+  //use memoization
+  if (m === 1 || n === 1) {
+    return 1;
+  }
+  return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+}
+
+uniquePaths(3, 7);
